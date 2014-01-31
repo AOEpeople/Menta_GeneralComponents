b70cddbbd821511f9e4a7bb2bac4b94ea5e6b31b
Agata <a.firlejczyk@macopedia.pl>
2014-01-31 14:54:24 +0100

    decode email subject

--- lib/GeneralComponents/ImapMail.php
+++ lib/GeneralComponents/ImapMail.php
@@ -101,7 +101,7 @@ class GeneralComponents_ImapMail extends Menta_Component_AbstractTest {
 	public function searchMailWithSubject($subject) {
 		$storage = $this->getStorage(true); // get new storage (triggering fresh lookup for new mails)
 		foreach ($storage as $idx => $message) { /* @var $message Zend_Mail_Message */
-			if (strpos($message->subject, $subject) !== false) {
+			if (strpos(iconv_mime_decode($message->subject, 0, 'UTF-8'), $subject) !== false) {
 				return $idx;
 			}
 		}
